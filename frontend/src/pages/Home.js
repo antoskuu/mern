245f1d4e3fe7303f60fdfc97eda987bfafce0db8
antoskuu <antoning.antonin@gmail.com>
2024-09-10 03:40:11 +0200

    updates boutton anmations

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,13 +3,24 @@ import axios from 'axios';
 import './Home.css';
 import fondImage from './fond.jpg'; // Importez l'image
 import fleche from './fleche.png'; // Importez l'image
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Importez les styles de Leaflet
 
 const Home = () => {
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [body, setBody] = useState('');
   const [sendAt, setSendAt] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const handleImageClick = (e) => {
+    e.stopPropagation(); // Arrête la propagation de l'événement de clic
+    setIsClicked(!isClicked);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,6 +37,13 @@ const Home = () => {
     <div className='page'>
       <h1 className='titre'>Votre capsule</h1>
       <form onSubmit={handleSubmit}>
+        <textarea 
+          placeholder="Enter body" 
+          className="custom-input"
+          value={body} 
+          onChange={(e) => setBody(e.target.value)} 
+          required 
+        />
         <input 
           type="email" 
           placeholder="Enter email" 
@@ -34,16 +52,10 @@ const Home = () => {
           required 
         />
         <input 
-          type="text" 
+          type="text"
           placeholder="Enter subject" 
           value={subject} 
           onChange={(e) => setSubject(e.target.value)} 
-          required 
-        />
-        <textarea 
-          placeholder="Enter body" 
-          value={body} 
-          onChange={(e) => setBody(e.target.value)} 
           required 
         />
         <input 
@@ -52,18 +64,26 @@ const Home = () => {
           onChange={(e) => setSendAt(e.target.value)} 
           required 
         />
-        
       </form>
-      <button onClick={handleSubmit}className="send-button" >
-          <img className="image-logo"src={fondImage} alt="Icon" />
-          <div className="button-text">
-            <span className="main-text">Envoyer la capsule</span>
-            <span className="sub-text">
-  {sendAt ? `Prévu pour le ${sendAt}` : "Date à définir"}
-            </span>            
-          </div>
-          <img className="fleche" src={fleche} />
-        </button>
+      <button onClick={handleSubmit} className="send-button">
+      <div onClick={handleImageClick} className={`image-logo ${isClicked ? 'clicked' : ''}`}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "300px", width: "100%" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+      </div>
+        <img onClick={handleImageClick} className={`image-logo ${isClicked ? 'clicked' : ''}`} src={fondImage} alt="Icon" />
+        <div className="button-text">
+          <span className="main-text">Envoyer la capsule</span>
+          <span className="sub-text">
+            {sendAt ? `Prévu pour le ${sendAt}` : "Date à définir"}
+          </span>            
+        </div>
+        <img className="fleche" src={fleche} alt="Fleche" />
+      </button>
+      
     </div>
   );
 }
